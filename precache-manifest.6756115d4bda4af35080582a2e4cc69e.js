a0570bc8a6b5c7360ebbd18f19bcf54c90425b8b
Abhay Baiju <abhay0198@gmail.com>
2020-08-30 17:30:23 +0530

    Updates

---
+++ precache-manifest.6756115d4bda4af35080582a2e4cc69e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a05a6330364770961f81b58bfbd5d04",
+    "url": "/hackyhelper/index.html"
+  },
+  {
+    "revision": "14f6b7fa2ec064df2952",
+    "url": "/hackyhelper/static/css/2.f69b3b01.chunk.css"
+  },
+  {
+    "revision": "14f6b7fa2ec064df2952",
+    "url": "/hackyhelper/static/js/2.6ab6f0b4.chunk.js"
+  },
+  {
+    "revision": "fff3e6f7249120469206d8b8bdd32074",
+    "url": "/hackyhelper/static/js/2.6ab6f0b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b95486b24397e788c043",
+    "url": "/hackyhelper/static/js/main.773e0f2d.chunk.js"
+  },
+  {
+    "revision": "28d4d4bd145c2d01be43",
+    "url": "/hackyhelper/static/js/runtime-main.f5a15a01.js"
+  }
+]);
